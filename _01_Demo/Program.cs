3d65435182caa12e2f7c887c4243dc234f9d7ac5
Mikk Muru <mikkmuru1@gmail.com>
2017-11-17 13:58:39 +0200

    last edit

--- _01_Demo/Program.cs
+++ _01_Demo/Program.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _01_Demo
+namespace _01_Small_program
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Display Text.
+            Console.WriteLine("Enter two random numbers between [1-100] and I will add them.");
+
+            //Ask for first number.
+            Console.Write("First number:");
+            string input = Console.ReadLine();
+            int num1 = int.Parse(input);
+
+            //Ask for second number.
+            Console.Write("Second number:");
+            string input2 = Console.ReadLine();
+            int num2 = int.Parse(input2);
+            
+            //Add two number and display the sum.
+            Console.Write("Sum is:"num1 + num2);
+            Console.ReadLine();
+            
         }
     }
 }
